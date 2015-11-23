340d429e28ed37f6909ec537191a44fff50c8222
linsalrob <raedwards@gmail.com>
2015-11-22 23:42:16 -0800

    initial release

--- scripts/assigned_functions_to_reactions.py
+++ scripts/assigned_functions_to_reactions.py
@@ -8,12 +8,23 @@ import os
 import sys
 
 parser=argparse.ArgumentParser(description='Convert an assigned_functions file to a list of roles')
-parser.add_argument('-a', help='assigned functions file', required=True)
+parser.add_argument('-a', help='assigned functions file')
+parser.add_argument("-r", help="roles only file (no pegs)")
 parser.add_argument('-v', help='verbose', action='store_true')
 args = parser.parse_args()
 
-af = parse.read_assigned_functions(args.a)
-rc = filters.roles_to_reactions(af.values())
+if args.a:
+    af = parse.read_assigned_functions(args.a)
+    roles = set(af.values())
+elif args.r:
+    roles =set()
+    with open(args.r, 'r') as f:
+        for l in f:
+            roles.add(l.strip())
+else:
+    sys.exit('Either -a or -r must be specified')
+
+rc = filters.roles_to_reactions(roles)
 reactions = set()
 for r in rc:
     reactions.update(rc[r])
