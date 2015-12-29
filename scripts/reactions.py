f16657b7b70cde9140909d0dc65b330d8f81c4b6
Robert Edwards <raedwards@gmail.com>
2015-12-29 15:10:36 +1000

    reaction counds

--- scripts/reactions.py
+++ scripts/reactions.py
@@ -11,6 +11,27 @@ import PyFBA
 
 compounds, reactions, enzymes = PyFBA.parse.compounds_reactions_enzymes('gram_negative')
 roles = PyFBA.parse.model_seed.roles()
+# some general information
+cloc = {}
+allc = set()
+for c in compounds:
+    allc.add(compounds[c].name)
+    cloc[compounds[c].location] = cloc.get(compounds[c].location, 0) + 1
+
+print("There are {} compounds".format(len(allc)))
+for l in cloc:
+    print("{} compounds are in {}".format(cloc[l], l))
+
+allr = set()
+rwithp = set()
+for r in reactions:
+    allr.add(reactions[r].name)
+    if reactions[r].number_of_enzymes() > 0:
+        rwithp.add(reactions[r].name)
+
+print("There are {} reactions".format(len(allr)))
+print("There are {} reactions associated with proteins".format(len(rwithp)))
+
 
 # find some roles that are in multiple complexes
 if False:
@@ -34,7 +55,7 @@ if False:
         print("\n")
 
 
-if True:
+if False:
     # looking for two different EC numbers asssociated with the same complex
     for c in enzymes:
         ec = set()
